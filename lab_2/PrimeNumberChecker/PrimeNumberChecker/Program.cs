e43653170293fa3b3fa77471132b4f496b5c6e00
Angelina-Grbchh <gorbachangelina1810@gmail.com>
2025-03-13 13:02:50 +0200

    changes

---
+++ lab_2/PrimeNumberChecker/PrimeNumberChecker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    static int Main(string[] args)
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out int number))
+            {
+                Console.Error.WriteLine("Помилка: Введене значення не є цілим числом.");
+                return 1; 
+            }
+
+            bool isPrime = IsPrime(number);
+            Console.WriteLine(isPrime);
+            return 0; 
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Помилка виконання: {ex.Message}");
+            return 2; 
+        }
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
