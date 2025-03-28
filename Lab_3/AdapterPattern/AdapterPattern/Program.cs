f910e4e5db772f312b4c27593419c656911f6c64
Angelina-Grbchh <gorbachangelina1810@gmail.com>
2025-03-28 22:51:28 +0200

    add lab3

---
+++ Lab_3/AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client.");
+            Console.WriteLine("But with adapter, the client can call its method.");
+            Console.WriteLine(target.GetRequest());
+        }
+    }
+}
