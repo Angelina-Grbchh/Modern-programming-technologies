fa9d46f6de303d32427d3f5203e3113efb3cee54
Angelina-Grbchh <gorbachangelina1810@gmail.com>
2025-04-25 22:39:33 +0300

    add lab

---
+++ lab_4/DeadlockDemo/DeadlockDemo.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using Xunit;
+using DeadlockDemo;
+
+namespace DeadlockDemo.Tests
+{
+    public class DeadlockTests
+    {
+        [Fact]
+        public void Deadlock_Should_Happen()
+        {
+            Thread thread1 = new Thread(Program.DoWork1);
+            Thread thread2 = new Thread(Program.DoWork2);
+
+            thread1.Start();
+            thread2.Start();
+
+            bool finished1 = thread1.Join(2000);
+            bool finished2 = thread2.Join(2000);
+
+            Assert.False(finished1 && finished2, "Дедлок не виник: обидва потоки завершилися");
+        }
+    }
+}
---
+++ lab_4/DeadlockDemo/DeadlockDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace DeadlockDemo
+{
+    public class Program
+    {
+        private static readonly object lockA = new object();
+        private static readonly object lockB = new object();
+
+        public static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(DoWork1);
+            Thread thread2 = new Thread(DoWork2);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Програма завершена.");
+        }
+
+        public static void DoWork1()
+        {
+            lock (lockA)
+            {
+                Console.WriteLine("Thread 1 захопив lockA");
+                Thread.Sleep(100);
+                lock (lockB)
+                {
+                    Console.WriteLine("Thread 1 захопив lockB");
+                }
+            }
+        }
+
+        public static void DoWork2()
+        {
+            lock (lockB)
+            {
+                Console.WriteLine("Thread 2 захопив lockB");
+                Thread.Sleep(100);
+                lock (lockA)
+                {
+                    Console.WriteLine("Thread 2 захопив lockA");
+                }
+            }
+        }
+    }
+}
